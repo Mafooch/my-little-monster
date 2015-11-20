7b7e43c3d60cc0aebbe66ec8be53f9890e7d0c50
Joshua Maffuccio <joshua.maffuccio@raizlabs.com>
2015-11-20 10:26:50 -0500

    animate golem

--- my-little-monster/ViewController.swift
+++ my-little-monster/ViewController.swift
@@ -10,9 +10,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var monsterImg: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        var imgArray = [UIImage]()
+        for var x = 1; x <= 4; x++ {
+            let img = UIImage(named: "idle\(x).png")
+//            this img is an optional but our imgArray is demanding something that's not an optional
+            imgArray.append(img!)
+//            we know there's a value here so we can force unwrap. if this crashes, it will do so in testing
+        }
+        
+        monsterImg.animationImages = imgArray
+        monsterImg.animationDuration = 0.8
+        monsterImg.animationRepeatCount = 0
+//        this means it will cycle through the animations infinitely
+        monsterImg.startAnimating()
     }
 }
 
